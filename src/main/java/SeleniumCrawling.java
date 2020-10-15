9f636a7a4b2976d791b1ae8b770a2750bbb0b8d2
Ryan Oh <madhandz5@gmail.com>
2020-10-15 17:40:24 +0900

    Selenium Crawling Practice/ BufferedWriter -> test.txt

--- src/main/java/SeleniumCrawling.java
+++ src/main/java/SeleniumCrawling.java
@@ -1,7 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
 
 public class SeleniumCrawling {
     private WebDriver driver;
@@ -11,8 +17,9 @@ public class SeleniumCrawling {
     public static final String WEB_DRIVER_PATH = "/usr/local/bin/chromedriver";
 
     private String baseUrl;
+    private String keyWord;
 
-    public SeleniumCrawling(){
+    public SeleniumCrawling() {
         super();
 
         System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
@@ -21,16 +28,37 @@ public class SeleniumCrawling {
         options.setCapability("ignoreProtectedModeSettings", true);
 
         driver = new ChromeDriver(options);
-        baseUrl = "https://www.naver.com";
+        baseUrl = "https://www.google.co.kr/";
+        keyWord = "JAVA";
     }
 
-    public void test(){
+    public void test() {
+        File file = new File("test.text");
+
         try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+
             driver.get(baseUrl);
-            System.out.println(driver.getTitle());
-        }catch (Exception e){
+            element = driver.findElement(By.name("q"));
+            element.sendKeys(keyWord);
+            element.submit();
+
+            List<WebElement> titles = driver.findElements(By.className("LC20lb"));
+            int index = 0;
+            bufferedWriter.append("검색 키워드 -> " + keyWord + "\n");
+            for (WebElement title : titles) {
+                index++;
+                System.out.println("제목 : " + title.getText() + "\n");
+                bufferedWriter.append("제목 : " + title.getText() + "\n");
+            }
+            System.out.println("총 검색 건수 : " + index);
+            bufferedWriter.append("총 검색 건수 : " + index);
+
+            bufferedWriter.flush();
+
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             driver.close();
         }
     }
